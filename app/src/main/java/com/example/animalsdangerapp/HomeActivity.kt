914dc2d353dd3aa6daf598416dd8046ea1c1ac0d
Célia <celia_torno@yahoo.com>
2022-07-29 19:46:20 +0200

    Animals Danger version 1.2

--- app/src/main/java/com/example/animalsdangerapp/HomeActivity.kt
+++ app/src/main/java/com/example/animalsdangerapp/HomeActivity.kt
@@ -2,6 +2,9 @@ package com.example.animalsdangerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
